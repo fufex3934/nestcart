bfb1ff027de6662ab4c1825e70d471842527ad93
Fufa Wakgari <fufawakgari174@gmail.com>
2026-03-17 09:57:38 +0300

    update app module is configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/app.config';
+import { validationSchema } from './config/validation.schema';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+
+    // Rate Limiting
+    ThrottlerModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        throttlers: [
+          {
+            ttl: config.get<number>('throttler.ttl') as number,
+            limit: config.get<number>('throttler.limit') as number,
+          },
+        ],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
